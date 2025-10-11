93e903796741dda08840108bdd09e4f0fd20744b
YasmineHaddad <yasminehaddad2323@gmail.com>
2025-10-11 14:35:46 +0100

    Initial commit: Spring Boot REST service with H2 database

---
+++ src/main/java/com/iset/ServiceOffreEmploiApplication.java
@@ -0,0 +1,13 @@
+package com.iset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOffreEmploiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOffreEmploiApplication.class, args);
+	}
+
+}
